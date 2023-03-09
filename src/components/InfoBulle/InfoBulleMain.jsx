0f109ad62e4bbb19060c2fac9fabf56afd835381
DamienLopvet <damienlopvet@gmail.com>
2023-03-09 10:34:06 +0100

    add message bubbles responsive

--- src/components/InfoBulle/InfoBulleMain.jsx
+++ src/components/InfoBulle/InfoBulleMain.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function InfoBulleMain() {
-  return (
-    <div className='infoBulleMain'>Files uploaded to the free server are automatically deleted after a few hours but you can test and upload text, jpeg, PNG, gif, webp and mp43 audio files etc.</div>
+  const [bubbleView, setBubbleView] = useState(true) 
+
+  setTimeout(() => {
+    setBubbleView(false)
+  }, 10000);
+  return (<>
+    {
+      bubbleView &&
+      <div className='infoBulleMain'>Files uploaded to the free server are automatically deleted after a few hours but you can test and upload text, jpeg, PNG, gif, webp and mp43 audio files etc.</div>
+      
+    }
+    </>
   )
 }
